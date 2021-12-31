e7fd47129d37060e5a924bfe3783b36e76b1a3b6
Kamalesh <kamalesh0101@gmail.com>
2022-01-01 05:06:44 +0530

    omz install sh to bash

--- zsh-configuration.sh
+++ zsh-configuration.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh | bash
