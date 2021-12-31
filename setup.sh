20a308d06b07d74fad41bb7b6e356885dcb410e0
Kamalesh <kamalesh0101@gmail.com>
2022-01-01 04:56:28 +0530

    omz testing

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 SOURCE=${BASH_SOURCE[0]}
 BASE_DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
 
-$BASE_DIR/pre-flight.sh
-$BASE_DIR/install-independents.sh
+# $BASE_DIR/pre-flight.sh
+# $BASE_DIR/install-independents.sh
 $BASE_DIR/zsh-configuration.sh
 $BASE_DIR/finalize.sh
