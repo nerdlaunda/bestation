a9c702a9f54d1f3cd0acd14e985d2f3ee5df7df2
Kamalesh <kamalesh0101@gmail.com>
2022-01-01 04:55:51 +0530

    omz basic

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ BASE_DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
 
 $BASE_DIR/pre-flight.sh
 $BASE_DIR/install-independents.sh
-$BASE_DIR/install-independents.sh
+$BASE_DIR/zsh-configuration.sh
 $BASE_DIR/finalize.sh
