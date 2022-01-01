7bcc418a68d5c6f52626c6038656741ffd3508d3
Kamalesh <kamalesh0101@gmail.com>
2022-01-01 06:04:44 +0530

    added omz plugins

--- finalize.sh
+++ finalize.sh
@@ -2,10 +2,12 @@
 SOURCE=${BASH_SOURCE[0]}
 BASE_DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
 
-
+echo "Copying .zshrc"
 cp $BASE_DIR/files/zshrc $HOME/.zshrc
+echo "Copying .p10k.zsh"
 cp $BASE_DIR/files/p10k.zsh $HOME/.p10k.zsh
+echo "Copying .bash_aliases"
 cp $BASE_DIR/files/bash_aliases $HOME/.bash_aliases
 
-
+echo "Adding path"
 echo "PATH=$PATH:$HOME/.local/bin" >> ~/.zshrc
--- zsh-configuration.sh
+++ zsh-configuration.sh
@@ -8,4 +8,10 @@ curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/instal
 # powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-cp $BASE_DIR/files/zshrc $HOME/.zshrc
+# Inatalling plugins
+
+echo "Installing plugins: "
+git clone https://github.com/jimeh/zsh-peco-history.git $HOME/.oh-my-zsh/custom/plugins/zsh-peco-history
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/agkozak/zsh-z $HOME/.oh-my-zsh/custom/plugins/zsh-z
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
